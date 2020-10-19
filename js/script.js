5549d3bf7e02db039f3eb702512ba18ddab61d75
=Joseph Gabriel <=josegabriel.jgs18@gmail.com>
2020-10-19 06:02:54 -0300

    Adicionando Responsividade

--- js/script.js
+++ js/script.js
@@ -1,1 +1,6 @@
-        //let rellax = new Rellax(".rellax")
+//let rellax = new Rellax(".rellax")
+
+window.addEventListener("load",()=>{
+        let loader = document.getElementById("loader")
+        loader.classList.add("finish")
+})
