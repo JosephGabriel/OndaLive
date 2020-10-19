6782d0143168d9420295e2a262a7ff668e9e66a4
=Joseph Gabriel <=josegabriel.jgs18@gmail.com>
2020-10-19 10:19:42 -0300

    Titulos corrigidos

--- js/script.js
+++ js/script.js
@@ -5,6 +5,37 @@ window.addEventListener("load",()=>{
         loader.classList.add("finish")
 })
 
+function countdown() {
+        let now = new Date()
+        let eventDate = new Date(2020, 11, 25)
+
+        let currentTime = now.getTime()
+        let eventTime = eventDate.getTime()
+
+        let remTime = eventTime - currentTime
+
+        let s = Math.floor(remTime / 1000)
+        let m = Math.floor(s / 60)
+        let h = Math.floor(m / 60)
+        let d = Math.floor(h / 24)
+
+        h %= 24
+        m %= 60
+        s %= 60
+
+        h = (h < 10) ? "0" + h : h
+        m = (m < 10) ? "0" + m : m
+        s = (s < 10) ? "0" + s : s
+
+        document.getElementById("days").innerText = d
+        document.getElementById("hours").innerText = h
+        document.getElementById("mins").innerText = m
+        document.getElementById("segs").innerText = s
+
+        setTimeout(countdown, 1000)
+
+    }
+
 window.addEventListener("load",()=>{
         setTimeout(countdown,1000)
 })
