dafb5a0e35bcb584ad2f53f73853ac8799de9eb5
=Joseph Gabriel <=josegabriel.jgs18@gmail.com>
2020-10-19 10:02:37 -0300

    Counter Dinamico Adicionado

--- js/script.js
+++ js/script.js
@@ -4,3 +4,7 @@ window.addEventListener("load",()=>{
         let loader = document.getElementById("loader")
         loader.classList.add("finish")
 })
+
+window.addEventListener("load",()=>{
+        setTimeout(countdown,1000)
+})
