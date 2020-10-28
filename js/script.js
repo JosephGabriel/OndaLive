a0e926d6bd228b9a1e7675d7e2228067b4c41802
Joseph Gabriel <josegabriel.jgs18@gmail.com>
2020-10-28 09:55:00 -0300

    Adicionando o adm

--- js/script.js
+++ js/script.js
@@ -1,7 +1,16 @@
 //let rellax = new Rellax(".rellax")
 
-window.addEventListener("load",()=>{
-        let loader = document.getElementById("loader")
-        loader.classList.add("finish")
-})
+window.addEventListener("load", () => {
+  let loader = document.getElementById("loader");
+  loader.classList.add("finish");
+});
+window.addEventListener("load", () => {
+  function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
 
+  /* Set the width of the side navigation to 0 */
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
+});
