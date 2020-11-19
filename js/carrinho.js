4e4509ca700d49b5d91fde10dd901dc212264dc1
Joseph Gabriel <josegabriel.jgs18@gmail.com>
2020-11-18 21:46:26 -0300

    Finalizando 43

--- js/carrinho.js
+++ js/carrinho.js
@@ -8,38 +8,50 @@ $("a.remove").click(function () {
 $("a.btn.continue").click(function () {
   $("li.items").show(400);
 });
-
 let menu = document.getElementById("menu-toggle");
-let nav = document.getElementById("nav-toggle");
 let drop = document.getElementById("dropdown");
 let dropImg = document.getElementById("dropdown-user-img");
 let imgDrop = document.getElementById("img-drop");
-let sidenav = document.getElementById("sidenav");
+let nav = document.getElementById("nav-toggle");
 let main = document.getElementById("main");
+let sidenav = document.getElementById("sidenav");
+let sidenavClose = document.getElementById("close-btn");
+let dropdown = document.getElementsByClassName("dropdown-btn");
 let dropRoom = document.getElementById("room-drop");
+let hallNav = document.getElementById("sidenav-program");
+let closeHall = document.getElementById("close-btn2");
+let roomBtnMobile = document.getElementById("room-dropdown-mobile");
 let roomDropdown = document.getElementById("room-dropdown");
+
+roomBtnMobile.addEventListener("click", () => {
+  hallNav.classList.toggle("activeS");
+  sidenav.classList.remove("activeS");
+  main.classList.remove("ops");
+});
+
+closeHall.addEventListener("click", () => {
+  hallNav.classList.remove("activeS");
+});
+
+main.addEventListener("click", () => {
+  dropRoom.classList.remove("active-dropdown");
+  drop.classList.remove("active-dropdown");
+  dropImg.classList.remove("active-dropdown");
+  main.classList.remove("ops");
+  hallNav.classList.remove("activeS");
+  sidenav.classList.remove("activeS");
+});
 
 nav.addEventListener("click", () => {
   sidenav.classList.toggle("activeS");
-});
-
-menu.addEventListener("click", () => {
-  drop.classList.toggle("active-dropdown");
-  dropImg.classList.remove("active-dropdown");
-  dropRoom.classList.remove("active-dropdown");
-});
-
-roomDropdown.addEventListener("click", () => {
   main.classList.remove("ops");
-  dropImg.classList.remove("active-dropdown");
-  drop.classList.remove("active-dropdown");
-  imgDrop.classList.remove("active-dropdown");
-  dropRoom.classList.toggle("active-dropdown");
+  hallNav.classList.remove("activeS");
 });
 
-// sidenav.addEventListener("click", () => {
-//   document.getElementById("nav-toggle").classList.toggle("activeS");
-// });
+sidenavClose.addEventListener("click", () => {
+  sidenav.classList.remove("activeS");
+  main.classList.remove("ops");
+});
 
 imgDrop.addEventListener("click", () => {
   drop.classList.remove("active-dropdown");
@@ -49,18 +61,18 @@ imgDrop.addEventListener("click", () => {
   main.classList.remove("ops");
 });
 
-// menu.addEventListener("click", () => {
-//   drop.classList.toggle("active-dropdown");
-//   dropImg.classList.remove("active-dropdown");
-//   roomDropdown.classList.remove("active-dropdown");
-//   dropRoom.classList.remove("active-dropdown");
-//   main.classList.remove("ops");
-// });
-
-main.addEventListener("click", () => {
-  document.getElementById("dropdown").classList.remove("active-dropdown");
-  document
-    .getElementById("dropdown-user-img")
-    .classList.remove("active-dropdown");
+menu.addEventListener("click", () => {
+  drop.classList.toggle("active-dropdown");
+  dropImg.classList.remove("active-dropdown");
+  roomDropdown.classList.remove("active-dropdown");
   dropRoom.classList.remove("active-dropdown");
+  main.classList.remove("ops");
+});
+
+roomDropdown.addEventListener("click", () => {
+  main.classList.remove("ops");
+  dropImg.classList.remove("active-dropdown");
+  drop.classList.remove("active-dropdown");
+  imgDrop.classList.remove("active-dropdown");
+  dropRoom.classList.toggle("active-dropdown");
 });
